861e895a5388248da5c235dea3ec6f0152493436
Bugingo Daniel <danielbugingo1@gmail.com>
2022-06-14 15:29:47 +0300

    Final update on the chat applications

--- room/views.py
+++ room/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .models import Room
+from .models import Message, Room
 
 
 @login_required
@@ -14,5 +14,6 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
 
-    return render(request, 'room/room.html', {'room': room})
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
